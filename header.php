f2f52a80fe8268366f556acf34010c711e4e85f4
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2020-02-03 12:25:33 +0100

    current

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head <?php language_attributes(); ?>>
+        <meta charset="<?php bloginfo( 'charset' );?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <?php 
+            wp_head();
+        ?>
+    </head>
