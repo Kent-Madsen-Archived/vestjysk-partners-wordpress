27cdf6ec95e05ad0c9252d2fee953ead7792a233
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2020-02-04 10:40:51 +0100

    added bootstrap framework

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php 
+    get_header();
+?>
+
+<!-- Page -->
+            <?php 
+            
+            ?>
+        </main>
+
+<?php
+    get_footer();
+?>
---
+++ singular.php
@@ -0,0 +1,14 @@
+<?php 
+    get_header();
+?>
+
+<!-- Singular -->
+        
+            <?php 
+            
+            ?>
+        </main>
+
+<?php
+    get_footer();
+?>
