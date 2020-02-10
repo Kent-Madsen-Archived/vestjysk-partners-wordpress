e71f6e634e47660f860792bde54ee55d33912553
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2020-02-10 13:48:39 +0100

    atlast, its more organised

--- inc/setup/include_styles.php
+++ inc/setup/include_styles.php
@@ -1,7 +1,8 @@
 <!-- PHP : Include Styles file -->
 <?php
 
-    function theme_enqueue_styles() {
+    function theme_enqueue_styles() 
+    {
         wp_enqueue_style( 'theme-styles', get_stylesheet_uri() ); // This is where you enqueue your theme's main stylesheet
         $custom_css = theme_get_customizer_css();
         wp_add_inline_style( 'theme-styles', $custom_css );
