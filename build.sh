3807ca2d16df953995dcede9cc3b135b23d62dc5
kent vejrup madsen <kent.vejrup.madsen@outlook.com>
2020-02-11 17:45:58 +0100

    removing frontpage

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 echo "Starting Building process of scss code"
 
-sass ./scss/style.scss ./style.css
+echo "[Main Template]"
+echo "- style.css"
+sass ./scss/style/style.scss ./style.css
 
-sh ./remove.sh
+
+echo "- print.css"
+sass ./scss/print/print.scss ./content/styles/print.css
+
+echo "- style-customizer.css"
+sass ./scss/style-customizer/style-customizer.scss ./content/styles/style-customizer.css
+
+echo "- style-editor.css"
+sass ./scss/style-editor/style-editor.scss ./content/styles/style-editor.css
+
+
+echo "[Components]"
+sass ./scss/components/test/test.scss ./content/styles/test.css
